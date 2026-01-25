0f40b97409b13819a6119f706a6471cab0d473c6
Siddhant <siddhantahire98@gmail.com>
2026-01-25 22:22:25 +0530

    Add LinkedIn link to header component if available

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -125,6 +125,19 @@ const Header = () => {
                   </Slide>
                 )}
 
+                {website?.website_linkedin && (
+                  <Slide triggerOnce direction="right">
+                    <a
+                      href={website.website_linkedin}
+                      className="d-flex align-items-center justify-content-center"
+                    >
+                      <span className="fa fa-linkedin">
+                        <i className="sr-only">LinkedIn</i>
+                      </span>
+                    </a>
+                  </Slide>
+                )}
+                
                 {website?.website_twitter && (
                   <Slide triggerOnce direction="right">
                     <a
